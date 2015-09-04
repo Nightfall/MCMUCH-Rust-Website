5ff05f6946148d21fd370a1ed7ab0a0b8acfd067
Skye M <skyem@hotmail.co.uk>
2015-09-04 20:20:40 +0100

    Start at interactive admin console
    Seems to be more effort than it's worth.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 extern crate iron;
 extern crate mount;
 extern crate staticfile;
+extern crate term;
 
 use std::path::Path;
+use std::thread;
 
 use iron::prelude::*;
 
 use mount::Mount;
 use staticfile::Static;
 
-fn main() {
-	let mut mount = Mount::new();
+mod console;
 
+fn main() {
+	// Print a welcome message
+	console::welcome();
+
+	let mut mount = Mount::new();
 	mount.mount("/", Static::new(Path::new("pages/")));
 
-	Iron::new(mount).http("localhost:3000").unwrap();
+	thread::spawn(move || {
+		Iron::new(mount).http("localhost:3000").unwrap();
+	});
+
+	console::run();
 }
